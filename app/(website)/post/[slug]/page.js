6c55adbd3ef98729f2e24ae5f127798d6d7ce7b9
spdewitt <spdewitt@outlook.com>
2025-01-01 19:57:41 -0600

    fixing canonicals

--- app/(website)/post/[slug]/page.js
+++ app/(website)/post/[slug]/page.js
@@ -8,7 +8,16 @@ export async function generateStaticParams() {
 
 export async function generateMetadata({ params }) {
   const post = await getPostBySlug(params.slug);
-  return { title: post.title };
+  return {
+    title: post.title,
+    description: post.excerpt ?? "Default description if none exists",
+    // If your post data has keywords, you could do something like:
+    // keywords: post.keywords ?? [],
+    alternates: {
+      // Notice the template literal to insert the slug dynamically
+      canonical: `https://www.nightsunstables.com/post/${params.slug}`
+    }
+  };
 }
 
 export default async function PostDefault({ params }) {
